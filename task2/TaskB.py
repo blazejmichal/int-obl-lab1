0285d55853cc2349aa128ff0e5ff24c520541648
Blazej <Blazej@DESKTOP-P8SE49K>
2020-10-21 22:59:00 +0200

    Poprawki.

--- task2/TaskB.py
+++ task2/TaskB.py
@@ -6,6 +6,6 @@ class Task2b:
     @staticmethod
     def execute():
         csvRow = '\n2010,460,555,405'
-        with open('../miasta.csv', 'a') as miasta:
+        with open('miasta.csv', 'a') as miasta:
             miasta.write(str(csvRow))
         Task2a.execute()
